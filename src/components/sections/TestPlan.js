1a05c2224a2e87371cc21e0ce766cf17dfd102a6
Ola Chrzan <ola.chrzan16@gmail.com>
2023-09-14 13:12:16 +0200

    Take lists out of paragraphs

--- src/components/sections/TestPlan.js
+++ src/components/sections/TestPlan.js
@@ -10,32 +10,26 @@ export const TestPlan = () => (
         https://radekjelen.github.io/currency-converter-React/
       </a>
     </p>
-    <p>
-      {'Tested elements:'}
-      <ul className="section__list">
-        <li>appearance based on the specification,</li>
-        <li>functioning and validation of elements,</li>
-        <li>event handling,</li>
-        <li>functioning of the software interface.</li>
-      </ul>
-    </p>
-    <p>
-      {'Non-testable elements:'}
-      <ul className="section__list">
-        <li>Code and elements of the internal system infrastructure</li>
-      </ul>
-    </p>
+    <span>Tested elements:</span>
+    <ul className="section__list">
+      <li>appearance based on the specification,</li>
+      <li>functioning and validation of elements,</li>
+      <li>event handling,</li>
+      <li>functioning of the software interface.</li>
+    </ul>
+    <span>Non-testable elements:</span>
+    <ul className="section__list">
+      <li>Code and elements of the internal system infrastructure</li>
+    </ul>
     <p>Testing approach: Exploratory, specification-based and software interface testing.</p>
     <p>Defined time: 3hrs., split into smaller sessions.</p>
-    <p>
-      {'Environment (most popular browser versions according to rankinkg.com on the day the tests started):'}
-      <ul className="section__list">
-        <li>PC Chrome,</li>
-        <li>PC FireFox,</li>
-        <li>PC Opera,</li>
-        <li>PC Edge.</li>
-      </ul>
-    </p>
+    <span>Environment (most popular browser versions according to rankinkg.com on the day the tests started):</span>
+    <ul className="section__list">
+      <li>PC Chrome,</li>
+      <li>PC FireFox,</li>
+      <li>PC Opera,</li>
+      <li>PC Edge.</li>
+    </ul>
     <Table name={'testPlan'} />
     <p>Additional time prepared for defect reporting and final report preparation.</p>
   </>
--- src/components/sections/TestReport.js
+++ src/components/sections/TestReport.js
@@ -1,16 +1,14 @@
 export const TestReport = () => (
   <>
     <span>ID: RzT-CC</span>
-    
+
     <h3 className="section__subheader">Introduction</h3>
-    <p className="section__paragraph">
-      {'Testing method:'}
-      <ol className="section__list">
-        <li>Tests referring to the provided specifications and appearance model of the application.</li>
-        <li>Exploratory (functional) tests checking the operation of the application on different web browsers.</li>
-      </ol>
-    </p>
-    
+    <span>Testing method:</span>
+    <ol className="section__list">
+      <li>Tests referring to the provided specifications and appearance model of the application.</li>
+      <li>Exploratory (functional) tests checking the operation of the application on different web browsers.</li>
+    </ol>
+
     <h3 className="section__subheader">Subject of tests</h3>
     <p className="section__paragraph">
       {'Version available on GitHub ('}
@@ -19,39 +17,37 @@ export const TestReport = () => (
       </a>
       {')'}
     </p>
-    
+
     <h3 className="section__subheader">Object features to not be tested</h3>
     <p className="section__paragraph">The semantic correctness of the code, its architecture and its decision points.</p>
-    
+
     <h3 className="section__subheader">Object features to be tested</h3>
     <p className="section__paragraph">
       The user-accessible elements of the application are tested from a functional, usability and aesthetic
       point of view.
     </p>
-    
+
     <h3 className="section__subheader">Test environment</h3>
     <p className="section__paragraph">
       Test environment: Google Chrome version 115.0.5, Mozilla FireFox version 116.0.2, Opera version
       101.0.1, Edge version 115.0.1.<br />Operating system name: Microsoft Windows 10 Pro 64bit.
     </p>
-    
+
     <h3 className="section__subheader">Activities, techniques and tools</h3>
     <p className="section__paragraph">Techniques: expropriation tests (functional tests)</p>
     <p>Tools: TestLink, Postman</p>
-    
+
     <h3 className="section__subheader">Pass/fail criteria for tests</h3>
-    <p className="section__paragraph">
-      {'For functional tests, a case is failed when at least one of the following effects has occurred:'}
-      <ul className="section__list">
-        <li>non-compliance of the application functionality with the expected result,</li>
-        <li>application suspension or crash,</li>
-        <li>incorrect error message,</li>
-        <li>unexpected operation of the application,</li>
-        <li>a message misleading the user,</li>
-        <li>negative message to the event.</li>
-      </ul>
-    </p>
-    
+    <span>For functional tests, a case is failed when at least one of the following effects has occurred:</span>
+    <ul className="section__list">
+      <li>non-compliance of the application functionality with the expected result,</li>
+      <li>application suspension or crash,</li>
+      <li>incorrect error message,</li>
+      <li>unexpected operation of the application,</li>
+      <li>a message misleading the user,</li>
+      <li>negative message to the event.</li>
+    </ul>
+
     <h3 className="section__subheader">Tasks to be performed</h3>
     <ol className="section__list">
       <li>Review the available documentation on the application under test.</li>
@@ -60,17 +56,17 @@ export const TestReport = () => (
       <li>Verification of layouts.</li>
       <li>Preparing the report.</li>
     </ol>
-    
+
     <h3 className="section__subheader">Functional tests and their results</h3>
     <p className="section__paragraph">
       Testing consisted of simultaneously learning about the application, performing exploratory tests and
       creating test documentation. The tester went to work with strictly defined goals, such as checking the
       functionality of the application based on best practice.
     </p>
-    
+
     <h3 className="section__subheader">Defects reported in the reporting system</h3>
     <p className="section__paragraph">The decision to correct the defects found should be made by the application tester.</p>
-    
+
     <h3 className="section__subheader">Defects detected</h3>
     <p className="section__paragraph">
       <span className="section__bold">#1</span> Validation error in the value field<br />
@@ -94,7 +90,7 @@ export const TestReport = () => (
       Priority: low.<br />
       In Firefox, the animated container frame loses its smooth transition.
     </p>
-    
+
     <h3 className="section__subheader">Summary</h3>
     <p className="section__paragraph">
       The application, has an eye-catching graphical interface. The user interface is clear and easy to read. Functions and validation are correct. The application's programming interface runs quickly and does not cause problems. The application works correctly on different types of browsers.
