0759df892f73e6b5a01a03eae3a6e8fc2c5a83e4
Ola Chrzan <ola.chrzan16@gmail.com>
2023-09-16 19:15:04 +0200

    Add collapse section button

--- src/components/dropDown/DropDown.js
+++ src/components/dropDown/DropDown.js
@@ -1,14 +1,26 @@
 import './dropDown.css';
 import '../sections/section.css';
+import { useRef } from 'react';
 
-export const DropDown = ({ name, nestingLevel, children }) => (
-  <details className="drop-down">
-    <summary className="drop-down__title-container">
-      {nestingLevel === 1 && (<h2 className="drop-down__title">{name}</h2>)}
-      {nestingLevel === 2 && (<h3 className="drop-down__title">{name}</h3>)}
-    </summary>
-    <div className='drop-down__content'>
-      {children}
-    </div>
-  </details>
-);
+export const DropDown = ({ name, nestingLevel, children }) => {
+  const detailsRef = useRef(null);
+
+  const collapseSection = () => {
+    detailsRef.current.removeAttribute('open');
+  };
+
+  return (
+    <details className="drop-down" ref={detailsRef}>
+      <summary className="drop-down__title-container">
+        {nestingLevel === 1 && (<h2 className="drop-down__title">{name}</h2>)}
+        {nestingLevel === 2 && (<h3 className="drop-down__title">{name}</h3>)}
+      </summary>
+      <div className='drop-down__content'>
+        {children}
+        <button className="drop-down__collapse-button" onClick={collapseSection}>
+          Collapse section
+        </button>
+      </div>
+    </details>
+  );
+};
