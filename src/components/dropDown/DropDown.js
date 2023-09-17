4f6a723a75df10340c3c42b0926f72e14b364613
Ola Chrzan <ola.chrzan16@gmail.com>
2023-09-17 13:14:03 +0200

    Change after element to component

--- src/components/dropDown/DropDown.js
+++ src/components/dropDown/DropDown.js
@@ -1,6 +1,7 @@
 import './dropDown.css';
 import '../sections/section.css';
 import { useRef } from 'react';
+import { ReactComponent as Arrow } from '../../icons/arrow.svg';
 
 export const DropDown = ({ name, nestingLevel, children }) => {
   const detailsRef = useRef(null);
@@ -14,6 +15,7 @@ export const DropDown = ({ name, nestingLevel, children }) => {
       <summary className="drop-down__title-container">
         {nestingLevel === 1 && (<h2 className="drop-down__title">{name}</h2>)}
         {nestingLevel === 2 && (<h3 className="drop-down__title">{name}</h3>)}
+        <Arrow className="drop-down__title-icon" />
       </summary>
       <div className='drop-down__content'>
         {children}
