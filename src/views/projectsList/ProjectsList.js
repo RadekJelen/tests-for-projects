0cb30cbdab15a139394ba1c866b8f238b10e5c77
Ola Chrzan <ola.chrzan16@gmail.com>
2023-09-17 15:39:50 +0200

    Change styles for project list page

--- src/views/projectsList/ProjectsList.js
+++ src/views/projectsList/ProjectsList.js
@@ -1,11 +1,13 @@
 import './projectsList.css';
 import { Link } from "react-router-dom";
+import { ReactComponent as Icon } from '../../icons/coins.svg';
 
-export const ProjectsList = () => (
+export const ProjectsList = () =>
   <main className='main'>
-    <h1>Choose project</h1>
-    <div className='tile'>
-      <Link className='tile__name' to="/currency-converter">Currency converter</Link>
-    </div>
+    <h1 className='main__header'>Choose project</h1>
+    <Link className='tile' to="/currency-converter">
+      <div className='tile__icon-wrapper'><Icon className='tile__icon' /></div>
+      <span className='tile__name'>Currency converter</span>
+    </Link>
   </main>
-);
+  ;
