7d51356a34f25bc333860efc10e06c553c45ac85
Ola Chrzan <ola.chrzan16@gmail.com>
2023-09-21 20:04:01 +0200

    Make switch for preconditions to add links and don't repeat content

--- src/components/sections/tests/TestCase.js
+++ src/components/sections/tests/TestCase.js
@@ -1,15 +1,29 @@
 import { Table } from "../table/Table";
+import { CC_PAGE_LINK, CC_API_RESULLTS, CC_API_LINK } from "../../../consts";
 
 export const TestCase = ({ data }) => {
   const tableHeaders = ["", "Step actions", "Expected Results", "Execution notes", "Execution status"]
   const { tableRows, summary, preconditions, importance, duration } = data;
   const tableData = { headers: tableHeaders, rows: tableRows };
 
+  const getPreconditions = () => {
+    const basicsText =  <>The page must be opened in any browser <a href={CC_PAGE_LINK} target='_blank'>{CC_PAGE_LINK}</a>.</>;
+
+    switch (preconditions) {
+      case 'basics':
+        return basicsText;
+      case 'withBrowsers':
+        return <>{basicsText} The browsers taking part in the tests are: Chrome, FireFox, Opera, MS Edge (in the latest versions).</>
+      case 'withApiLinks':
+        return <>{basicsText} Data for checking results <a href={CC_API_RESULLTS} target='_blank'>{CC_API_RESULLTS}</a>. Api website <a href={CC_API_LINK} target='_blank'>{CC_API_LINK}</a></>
+    }
+  }
+
   return (
     <>
       <p><span className="test__subtitle">Author: </span>RJ</p>
       <p><span className="test__subtitle">Summary: </span><br />{summary}</p>
-      <p><span className="test__subtitle">Preconditions: </span><br />{preconditions}</p>
+      <p><span className="test__subtitle">Preconditions: </span><br />{getPreconditions()}</p>
       <Table data={tableData} />
       <p>
         <span className="test__subtitle">Execution type: </span>Manual<br />
--- src/consts.js
+++ src/consts.js
@@ -1,1 +1,3 @@
 export const CC_PAGE_LINK = 'https://radekjelen.github.io/currency-converter-React/';
+export const CC_API_RESULLTS = 'https://api.exchangerate.host/latest?base=PLN';
+export const CC_API_LINK = 'https://exchangerate.host/#/';
