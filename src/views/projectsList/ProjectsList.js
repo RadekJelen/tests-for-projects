36071f213c9105a324876212fc3bb3d033f6f080
Ola Chrzan <ola.chrzan16@gmail.com>
2023-09-21 18:56:37 +0200

    Add work-in-progress tile

--- src/views/projectsList/ProjectsList.js
+++ src/views/projectsList/ProjectsList.js
@@ -1,13 +1,20 @@
 import './projectsList.scss';
 import { Link } from "react-router-dom";
-import { ReactComponent as Icon } from '../../icons/coins.svg';
+import { ReactComponent as Coins } from '../../icons/coins.svg';
+import { ReactComponent as Pen } from '../../icons/pen.svg';
 
 export const ProjectsList = () =>
   <main className='main'>
     <h1 className='main__header'>Choose project</h1>
-    <Link className='tile' to="/currency-converter">
-      <div className='tile__icon-wrapper'><Icon className='tile__icon' /></div>
-      <span className='tile__name'>Currency converter</span>
-    </Link>
+    <div className='main__tile-container'>
+      <Link className='tile' to="/currency-converter">
+        <div className='tile__icon-wrapper'><Coins className='tile__icon' /></div>
+        <span className='tile__name'>Currency converter</span>
+      </Link>
+      <div className='tile tile--work'>
+        <div className='tile__icon-wrapper'><Pen className='tile__icon' /></div>
+        <span className='tile__name'>work in progress...</span>
+      </div>
+    </div>
   </main>
   ;
